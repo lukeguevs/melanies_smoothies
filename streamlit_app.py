3b6d7fa298eb292dfe1044a7abb79181109fb848
Lucas Guevremont <144279949+lukeguevs@users.noreply.github.com>
2025-05-12 11:56:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ session = st.connection('snowflake').session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
 # #st.dataframe(data = my_dataframe, use_container_width=True)
 # st.stop()
 
